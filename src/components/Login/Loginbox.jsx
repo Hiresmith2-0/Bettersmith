72c92f821aeaebd624f02b6b1404e40b5ceabeb8
Wayne Tsai <wtsai45@gmail.com>
2022-06-29 17:22:06 -0700

    added CSS to login/signup/home

--- src/components/Login/Loginbox.jsx
+++ src/components/Login/Loginbox.jsx
@@ -5,14 +5,12 @@ export default function LoginBox () {
   return (
     <div id='login-mainbox'>
       <h2>BetterSmith</h2>
-      <h3>Sign In</h3>
-      <h3>Hiresmith</h3>
         <div id ='signin-form'>
           <form method='POST' action='/login'>
-            <input type='text' placeholder='username' />
-            <input type='password' placeholder='password' />
-            <input type='submit' value="login"/>
-            {/* <a href='../../pages/Signup/Signup.jsx'>Sign up</a> */}
+            <input type='text' placeholder='username' id='username' /><br/>
+            <input type='password' placeholder='password' id='password'/><br/>
+            <input type='submit' value="Log In" id='loginbtn'/>
+            <a href='../../pages/Signup/Signup.jsx' id='signuplink'>Sign up</a>
             {/* <button id='sign-in'>Sign In</button> */}
             {/* <button id='sign-up'>Sign Up</button> */}
           </form>
--- src/components/Signup/SignupBox.jsx
+++ src/components/Signup/SignupBox.jsx
@@ -40,28 +40,32 @@ export default function SignupBox (props) {
       <h2>BetterSmith</h2>
       <h3>Sign Up</h3>
       <div id='signup-form'>
-        <form method='POST' action='/signup'>
+        <form method='POST' action='/signup' id="signupboxform">
           <input
             type='text'
             placeholder='First Name'
             onChange={firstNameOnChange}
+            id="signupinput"
           />
           <input
             type='text'
             placeholder='Last Name'
             onChange={lastNameOnChange}
+            id="signupinput"
           />
           <input
             type='text'
             placeholder='Username'
             onChange={usernameOnChange}
+            id="signupinput"
           />
           <input
             type='password'
             placeholder='Password'
             onChange={passwordOnChange}
+            id="signupinput"
           />
-          <input type='submit' value='signup' onClick={saveUser} />
+          <input id="signupinput" type='submit' value='signup' onClick={saveUser} />
           {/* <a href='../../pages/Login/Login.jsx'>Log in</a> */}
         </form>
       </div>
